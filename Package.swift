f86d6c2cef471e33c7e6cc4887491675a8b95aea
Lister <L547837@163.com>
2021-04-17 16:20:48 +0800

    Update Package.swift
    修改包名

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "lister-common",
+    name: "ListerCommon",
     platforms: [
        .macOS(.v11),
        .iOS(.v13),
@@ -14,8 +14,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "lister-common",
-            targets: ["lister-common"]),
+            name: "ListerCommon",
+            targets: ["ListerCommon"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -25,10 +25,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "lister-common",
+            name: "ListerCommon",
             dependencies: []),
         .testTarget(
-            name: "lister-commonTests",
-            dependencies: ["lister-common"]),
+            name: "ListerCommonTests",
+            dependencies: ["ListerCommon"]),
     ]
 )
