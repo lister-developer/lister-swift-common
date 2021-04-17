8418bb8f652fb6b4e74b5676a875b62dc78abedf
Lister <L547837@163.com>
2021-04-17 17:07:04 +0800

    Update DateUtil.swift

--- Sources/ListerCommon/Utils/DateUtil.swift
+++ Sources/ListerCommon/Utils/DateUtil.swift
@@ -12,7 +12,7 @@ struct DateUtil {
     /// 标准日期格式化为 年月日时分秒
     /// - Parameter date: 日期时间
     /// - Returns: 日期时间字符串
-    static func defaultDate2String(_ date: Date?) -> String {
+    public static func defaultDate2String(_ date: Date?) -> String {
         guard date != nil else { return "" }
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
